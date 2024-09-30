4c7480b79f419b9474664129ebf4858627d39f67
Samueelx <gsammygitau@gmail.com>
2024-09-30 20:38:24 +0300

    Add card components an fetch recipes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,45 @@
+import { Card, CardContent, CardDescription, CardHeader, CardFooter, CardTitle } from "@/components/ui/card"
 
-export default function Home() {
+interface Recipe {
+  title: string,
+  image: string,
+  time: number,
+  description: string,
+  vegan: boolean,
+  id: string,
+};
+
+async function getRecipes(): Promise<Recipe[]>{
+  const result = await fetch('http://localhost:4000/recipes');
+
+  return result.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
+
   return (
     <main>
-      Home
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map(recipe => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              {/* Avatar */}
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <button>View recipe</button>
+              {recipe.vegan && <p>Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </main>    
   );
 }
